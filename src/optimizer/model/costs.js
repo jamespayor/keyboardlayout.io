e7cd73fa4f812a592ea90cd1ea3cb711a48af0e9
James Payor <james@payor.com.au>
2018-07-04 21:17:46 -0700

    Small cost model improvement. Fix branch.

--- src/optimizer/model/costs.js
+++ src/optimizer/model/costs.js
@@ -12,7 +12,7 @@
 
 export const baseCosts = [
                // Pinky Ring Mid Index Index  Index Index Mid Ring Pinky Pinky
-/*   Top row */ [  6.0, 3.0, 2.1, 1.8, 2.5,     2.5, 1.8, 2.1, 3.0, 6.0, 8.0 ],
+/*   Top row */ [  6.0, 2.8, 1.9, 2.0, 2.5,     2.5, 2.0, 1.9, 2.8, 6.0, 8.0 ],
 /* Upper row */ [  3.2, 2.5, 1.6, 1.3, 2.0,     2.0, 1.3, 1.6, 2.5, 3.2, 6.5 ],
 /*  Home row */ [  2.6, 1.6, 1.2, 1.0, 1.4,     1.4, 1.0, 1.2, 1.6, 2.6, 4.7 ],
 /* Lower row */ [  3.0, 2.2, 2.4, 1.2, 1.8,     1.8, 1.2, 2.4, 2.2, 3.0, 6.0 ],
@@ -76,7 +76,7 @@ export function bigramCost(firstRowIndex, firstKeyIndex, secondRowIndex, secondK
     return rowCostMultiplier * columnCostMultiplier * baseCost;
   }
 
-  if (secondFinger === 4) {
+  if (secondFinger === 3) {
     // Second finger is pinky.
 
     if (firstFinger === 0) {
