c7810920bbcb311124bf984a78517945534f411f
James Payor <james@payor.io>
2019-10-19 17:00:35 -0700

    Checkpoint

--- src/optimizer/index.js
+++ src/optimizer/index.js
@@ -1,11 +1,11 @@
 import bruteForce from './brute-force';
-
-// TODO TODO
-import {startOptimization, stopOptimization} from "../redux/actions/optimizer/running";
-
 let optimizerCurrentlyRunning = false;
 
-export default function manageOptimization(reduxState, updateCandidateCallback) {
+function startOptimization(updateCandidateCallback) {
+  console.log("Started optimization!");
+}
+
+export default function optimizerListener(reduxState, updateCandidateCallback) {
   const {keyboard, optimizer: {running: optimizerRequestedRunning, selection}} = reduxState;
   if (optimizerRequestedRunning && !optimizerCurrentlyRunning) {
     startOptimization(updateCandidateCallback);
